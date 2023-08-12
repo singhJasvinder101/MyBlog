1611de2526a733794e3244bfbdf5d059069b10cf
Jasvinder <mrkaransingh3593@gmail.com>
2023-08-12 12:25:48 +0530

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Credentials', 'true');
-    app.options('*', cors(corsOptions)); // Pre-flight request
+    app.options('*', cors(corsOptions));
     next();
 });
 
