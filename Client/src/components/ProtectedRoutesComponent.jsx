3b7a784a5b68d359d004c7236927de7b8d333891
Jasvinder <mrkaransingh3593@gmail.com>
2023-08-13 12:57:49 +0530

    fixeing issues and frontend improvement

--- Client/src/components/ProtectedRoutesComponent.jsx
+++ Client/src/components/ProtectedRoutesComponent.jsx
@@ -2,23 +2,26 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Outlet } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logOutUser, setRedxUserState } from "../../redux/slices/loginRegisterSlice";
 const ProtectedRoutesComponent = () => {
     const [isAuth, setIsAuth] = useState(false);
-    
+    const userInfo = useSelector((state) => state.loginRegister.userInfo);
+
     const navigate = useNavigate();
     const apiUrl = import.meta.env.VITE_API_URI;
     const dispatch = useDispatch()
-    
+
     useEffect(() => {
         const check_token = async () => {
             try {
-                axios.defaults.withCredentials = true;
-                const { data } = await axios.get(`${apiUrl}/api/get-token`, {
-                    withCredentials: true,
-                })
-                if (data.token) {
+                // axios.defaults.withCredentials = true;
+                // const { data } = await axios.get(`${apiUrl}/api/get-token`, {
+                //     withCredentials: true,
+                // })
+                const data = userInfo
+                // if (data.token) {
+                if (data.name) {
                     setIsAuth(true)
                 } else {
                     setIsAuth(false)
@@ -36,12 +39,12 @@ const ProtectedRoutesComponent = () => {
     }, [isAuth]);
 
     if (isAuth === undefined) return <LoginPage />;
-    if (isAuth) {
-        return <Outlet />;
-    } else {
-        navigate("/login");
+    if (!isAuth) {
+        navigate('/login');
         return null;
     }
+
+    return <Outlet />;
 };
 
 export default ProtectedRoutesComponent;
