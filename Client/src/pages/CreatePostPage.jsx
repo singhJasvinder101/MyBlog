ad38fd0ff38b234e5e6c53d3b7f93e0699116964
Jasvinder <mrkaransingh3593@gmail.com>
2023-08-31 13:27:09 +0530

    new commit

--- Client/src/pages/CreatePostPage.jsx
+++ Client/src/pages/CreatePostPage.jsx
@@ -109,6 +109,7 @@ const TextEditor = () => {
                 { indent: "+1" },
                 { align: [] }
             ],
+            ["link"]
             [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
         ],
     };
@@ -151,7 +152,7 @@ const TextEditor = () => {
         // console.log(title, description, body_html, tags, images, author)
     }
 
-    console.log(tags)
+    // console.log(tags)
 
     return (
         <div className='create-post-page'>
@@ -199,7 +200,7 @@ const TextEditor = () => {
                         style={{ height: "220px" }}
                     />
                     <div className="publish-post mt-5">
-                        <button type='submit' style={{ height: '3rem' }} className='btn btn-primary'>
+                        <button type='submit' style={{ height: '3rem', marginTop: '4rem' }} className='btn btn-primary'>
                             {articleStatePublished ? 'Published \uD83C\uDF89' : 'Publish Article'}
                             {loaderState ? (
                                 <Spinner
