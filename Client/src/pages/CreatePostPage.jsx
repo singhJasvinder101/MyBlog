bdb7d6b32d963a6ba5f71e5b868d255f56feb631
Aryan <aryansharma220318@gmail.com>
2024-05-27 20:53:26 +0530

    added a button to upload file

--- Client/src/pages/CreatePostPage.jsx
+++ Client/src/pages/CreatePostPage.jsx
@@ -7,8 +7,8 @@ import 'prismjs/components/prism-javascript';
 import "prismjs/themes/prism-tomorrow.css";
 
 import { CloudinaryContext, Image } from 'cloudinary-react';
-import { Spinner } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Spinner } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { Alert } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 const apiUrl = import.meta.env.VITE_API_URI;
@@ -18,19 +18,28 @@ import axios from 'axios';
 
 const TextEditor = () => {
     const [uploadedImagePublicId, setUploadedImagePublicId] = useState(null);
-    const [uploading, setUploading] = useState(false)
+    const [uploading, setUploading] = useState(false);
     const [tagInput, setTagInput] = useState('');
-    const [tags, setTags] = useState([])
-    const [imageUrl, setImageUrl] = useState("https://res.cloudinary.com/practicaldev/image/fetch/s--gIvrKWQi--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5rbe6wwa5mcc2q4me43s.png")
-    const [loaderState, setLoaderState] = useState(false)
-    const [articleStatePublished, setArticleStatePublished] = useState(false)
-    const textAreaRef = useRef(null)
-    const [content, setContent] = useState('')
-    const body_html = content
+    const [tags, setTags] = useState([]);
+    const [imageUrl, setImageUrl] = useState("https://res.cloudinary.com/practicaldev/image/fetch/s--gIvrKWQi--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5rbe6wwa5mcc2q4me43s.png");
+    const [loaderState, setLoaderState] = useState(false);
+    const [articleStatePublished, setArticleStatePublished] = useState(false);
+    const textAreaRef = useRef(null);
+    const [content, setContent] = useState('');
+    const body_html = content;
 
+    const user = useSelector(state => state.userLoggedIn.userInfo);
 
-    const uploadImage = async (e) => {
-        setUploading(true)
+    useEffect(() => {
+        Prism.highlightAll();
+    }, [content]);
+
+    useEffect(() => {
+        // console.log(tags);
+    }, [tags]);
+
+    const handleFileChange = async (e) => {
+        setUploading(true);
         const file = e.target.files[0];
         const formData = new FormData();
         formData.append('file', file);
@@ -43,45 +52,46 @@ const TextEditor = () => {
             }
         );
         const data = await response.json();
-        setUploading(false)
+        setUploading(false);
         setUploadedImagePublicId(data.public_id);
-        setImageUrl(data.url)
+        setImageUrl(data.url);
         // console.log(data);
     };
 
+    const triggerFileInput = () => {
+        document.getElementById('inputGroupFile01').click();
+    };
 
-    useEffect(() => {
-        // console.log(tags);
-    }, [tags]);
+    const handleUploadClick = () => {
+        const fileInput = document.getElementById('inputGroupFile01');
+        if (fileInput.files.length > 0) {
+            handleFileChange({ target: { files: fileInput.files } });
+        } else {
+            alert('Please select a file first.');
+        }
+    };
 
     const tagsHandler = (e) => {
         if (e.key === 'Enter') {
             e.preventDefault();
-            setTags(tagInput.split(','))
             setTags(tagInput.split(',').map(tag => tag.trim()));
             setTagInput('');
         }
-    }
-
-    const user = useSelector(state => state.userLoggedIn.userInfo)
-    // console.log(user)
+    };
 
     const blogPostApiRequest = async (title, description, body_html, tags, images, author) => {
-        setLoaderState(true)
+        setLoaderState(true);
         const { data } = await axios.post(`${apiUrl}/api/blogs/createPost`, {
             title, description, body_html, tags, images, author
         }, {
             withCredentials: true,
-        })
-        setLoaderState(false)
-        setArticleStatePublished(true)
-        return data
-    }
-
+        });
+        setLoaderState(false);
+        setArticleStatePublished(true);
+        return data;
+    };
 
     const handleProcedureContentChange = (newContent) => {
-        // console.log("content---->", newContent);
-
         const highlightedContent = newContent.replace(
             /<blockquote>([\s\S]*?)<\/blockquote>/g,
             (match, p1) => {
@@ -96,7 +106,6 @@ const TextEditor = () => {
         setContent(highlightedContent);
     };
 
-
     var modules = {
         toolbar: [
             [{ size: ["small", false, "large", "huge"] }],
@@ -110,8 +119,8 @@ const TextEditor = () => {
                 { indent: "+1" },
                 { align: [] }
             ],
-            ["link"]
-            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
+            ["link"],
+            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }]
         ],
     };
 
@@ -122,39 +131,27 @@ const TextEditor = () => {
         "link", "image", "align", "size",
     ];
 
-    useEffect(() => {
-        Prism.highlightAll();
-    }, [content]);
-
-
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         e.stopPropagation();
 
-        const options = {
-            wordwrap: 130,
-        };
-
         const form = e.currentTarget.elements;
-        const title = form.title.value
+        const title = form.title.value;
         const purifiedDescription = DOMPurify.sanitize(content, {
             ALLOWED_TAGS: [],
         });
-        const descriptionString = purifiedDescription.slice(0, 90).replace(/\n/g, '');;
-        const description = descriptionString
+        const descriptionString = purifiedDescription.slice(0, 90).replace(/\n/g, '');
+        const description = descriptionString;
 
-        const images = [{ path: imageUrl }]
-        const author = user.name + user.lastname
+        const images = [{ path: imageUrl }];
+        const author = `${user.name} ${user.lastname}`;
         blogPostApiRequest(title, description, body_html, tags, images, author)
             .then(res => {
                 // console.log(res)
-                window.location.href = "/"
+                window.location.href = "/";
             })
-            .catch(err => console.log(err))
-        // console.log(title, description, body_html, tags, images, author)
-    }
-
-    // console.log(tags)
+            .catch(err => console.log(err));
+    };
 
     return (
         <div className='create-post-page'>
@@ -165,7 +162,7 @@ const TextEditor = () => {
                     <input
                         id='tags'
                         className='tags-input my-4 px-4 py-2'
-                        placeholder='tags with commas,  then press Enter'
+                        placeholder='tags with commas, then press Enter'
                         onKeyPress={tagsHandler}
                         value={tagInput}
                         onChange={(e) => setTagInput(e.target.value)}
@@ -178,18 +175,27 @@ const TextEditor = () => {
                     </div>
 
                     <div className="cover-image-input text-left">
-                        <label title='add photo of 100:42 ratio' className="input-text" htmlFor="inputGroupFile01">Select you cover image {uploading ? <Spinner /> : null}</label>
-                        <input type="file" id='inputGroupFile01'
+                        <label title='add photo of 100:42 ratio' className="input-text" htmlFor="inputGroupFile01">
+                            Select your cover image {uploading ? <Spinner /> : null}
+                        </label>
+                        <input
+                            type="file"
+                            id='inputGroupFile01'
                             className='form'
                             style={{ visibility: 'hidden' }}
-                            onChange={uploadImage} />
+                            onChange={handleFileChange}
+                        />
+                        <button type="button" onClick={triggerFileInput} className="btn btn-secondary mx-2">Select Image</button>
+                        <button type="button" onClick={handleUploadClick} className="btn btn-primary mx-2">Upload Image</button>
                     </div>
+
                     {uploadedImagePublicId ? (
                         <CloudinaryContext cloudName="dfdmyewjs" className='my-3'>
                             <Image className='cloudinary-image' publicId={uploadedImagePublicId} height='200' width="400" crop="scale" />
                         </CloudinaryContext>
-                    ) : (<Image className='d-block my-3 posted-img' height='20%' width="100%" crop="scale" src='https://res.cloudinary.com/practicaldev/image/fetch/s--gIvrKWQi--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5rbe6wwa5mcc2q4me43s.png' />)}
-
+                    ) : (
+                        <Image className='d-block my-3 posted-img' height='20%' width="100%" crop="scale" src='https://res.cloudinary.com/practicaldev/image/fetch/s--gIvrKWQi--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5rbe6wwa5mcc2q4me43s.png' />
+                    )}
                 </div>
                 <div className="text-editor container">
                     <ReactQuill
@@ -211,13 +217,13 @@ const TextEditor = () => {
                                     animation="border"
                                     size="sm"
                                     role="status"
-                                    aria-hidden="true" />
+                                    aria-hidden="true"
+                                />
                             ) : ""}
                         </button>
                     </div>
                     <div className="preview my-4">
                         <p>Preview:</p>
-                        {/* <blockquote className='text-dark'>{content}</blockquote> */}
                         <div
                             className='language-javascript'
                             dangerouslySetInnerHTML={{ __html: content }}
@@ -225,7 +231,7 @@ const TextEditor = () => {
                     </div>
                 </div>
             </form>
-        </div >
+        </div>
     );
 };
 
