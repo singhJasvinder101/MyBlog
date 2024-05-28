9501d65e2d90b8f6426992f93dbeb6ecd325d894
Aryan <aryansharma220318@gmail.com>
2024-05-28 18:56:47 +0530

    made requested changes

--- Client/src/pages/CreatePostPage.jsx
+++ Client/src/pages/CreatePostPage.jsx
@@ -28,62 +28,72 @@ const TextEditor = () => {
     const [content, setContent] = useState('');
     const body_html = content;
 
-    const uploadImage = async (e) => {
-        setUploading(true);
-        const file = e.target.files[0];
-        const formData = new FormData();
-        formData.append('file', file);
-        formData.append('upload_preset', 'hndevrtd');
-        try {
-            const response = await axios.post(
-                `https://api.cloudinary.com/v1_1/dfdmyewjs/image/upload`,
-                formData
-            );
-            const data = response.data;
-            setUploading(false);
-            setUploadedImagePublicId(data.public_id);
-            setImageUrl(data.url);
-        } catch (error) {
-            console.error("Error uploading image:", error);
-            setUploading(false);
-        }
-    };
+    const user = useSelector(state => state.userLoggedIn.userInfo);
 
     useEffect(() => {
         Prism.highlightAll();
     }, [content]);
 
+    useEffect(() => {
+        // console.log(tags);
+    }, [tags]);
+
+    const handleFileChange = async (e) => {
+        setUploading(true);
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('upload_preset', 'hndevrtd');
+        const response = await fetch(
+            `https://api.cloudinary.com/v1_1/dfdmyewjs/image/upload`,
+            {
+                method: 'POST',
+                body: formData,
+            }
+        );
+        const data = await response.json();
+        setUploading(false);
+        setUploadedImagePublicId(data.public_id);
+        setImageUrl(data.url);
+        // console.log(data);
+    };
+
+    const triggerFileInput = () => {
+        document.getElementById('inputGroupFile01').click();
+    };
+
+    const handleUploadClick = () => {
+        const fileInput = document.getElementById('inputGroupFile01');
+        if (fileInput.files.length > 0) {
+            handleFileChange({ target: { files: fileInput.files } });
+        } else {
+            alert('Please select a file first.');
+        }
+    };
+
     const tagsHandler = (e) => {
         if (e.key === 'Enter') {
             e.preventDefault();
-            const newTags = tagInput.split(',').map(tag => tag.trim());
-            setTags([...tags, ...newTags]);
+            setTags(tagInput.split(',').map(tag => tag.trim()));
             setTagInput('');
         }
     };
 
-    const user = useSelector(state => state.userLoggedIn.userInfo);
-
     const blogPostApiRequest = async (title, description, body_html, tags, images, author) => {
         setLoaderState(true);
-        try {
-            const { data } = await axios.post(`${apiUrl}/api/blogs/createPost`, {
-                title, description, body_html, tags, images, author
-            }, {
-                withCredentials: true,
-            });
-            setLoaderState(false);
-            setArticleStatePublished(true);
-            return data;
-        } catch (error) {
-            console.error("Error creating blog post:", error);
-            setLoaderState(false);
-        }
+        const { data } = await axios.post(`${apiUrl}/api/blogs/createPost`, {
+            title, description, body_html, tags, images, author
+        }, {
+            withCredentials: true,
+        });
+        setLoaderState(false);
+        setArticleStatePublished(true);
+        return data;
     };
 
     const handleProcedureContentChange = (newContent) => {
         const highlightedContent = newContent.replace(
-            /<pre><code class="language-javascript">([\s\S]*?)<\/code><\/pre>/g,
+            /<blockquote>([\s\S]*?)<\/blockquote>/g,
             (match, p1) => {
                 const highlightedCode = Prism.highlight(
                     p1,
@@ -96,7 +106,7 @@ const TextEditor = () => {
         setContent(highlightedContent);
     };
 
-    const modules = {
+    var modules = {
         toolbar: [
             [{ size: ["small", false, "large", "huge"] }],
             ["bold", "italic", "underline", "strike", "blockquote"],
@@ -110,11 +120,11 @@ const TextEditor = () => {
                 { align: [] }
             ],
             ["link"],
-            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
+            [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }]
         ],
     };
 
-    const formats = [
+    var formats = [
         "header", "height", "bold", "italic",
         "underline", "strike", "blockquote",
         "list", "color", "bullet", "indent",
@@ -137,13 +147,10 @@ const TextEditor = () => {
         const author = `${user.name} ${user.lastname}`;
         blogPostApiRequest(title, description, body_html, tags, images, author)
             .then(res => {
+                // console.log(res)
                 window.location.href = "/";
             })
-            .catch(err => console.error(err));
-    };
-
-    const triggerFileInput = () => {
-        document.getElementById('inputGroupFile01').click();
+            .catch(err => console.log(err));
     };
 
     return (
@@ -160,33 +167,34 @@ const TextEditor = () => {
                         value={tagInput}
                         onChange={(e) => setTagInput(e.target.value)}
                     />
+
                     <div className="post-tags">
                         {tags && tags.map((tag, i) => (
                             <span key={i} className='post-tag d-inline-block'># {tag}</span>
                         ))}
                     </div>
+
                     <div className="cover-image-input text-left">
-                        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                            <label title='add photo of 100:42 ratio' className="input-text">
-                                Select your cover image
-                            </label>
-                            {uploading && <Spinner animation="border" />}
-                        </div>
+                        <label style={{display:'inline-flex',alignItems:'center'}} title='add photo of 100:42 ratio' className="input-text" >
+                            Select your cover image {uploading ? <Spinner /> : null}
+                        </label>
                         <input
                             type="file"
                             id='inputGroupFile01'
                             className='form'
                             style={{ visibility: 'hidden' }}
-                            onChange={uploadImage}
+                            onChange={handleFileChange}
                         />
                         <button type="button" onClick={triggerFileInput} className="btn btn-secondary mx-2">Select Image</button>
+                        <button type="button" onClick={handleUploadClick} className="btn btn-primary mx-2">Upload Image</button>
                     </div>
+
                     {uploadedImagePublicId ? (
                         <CloudinaryContext cloudName="dfdmyewjs" className='my-3'>
                             <Image className='cloudinary-image' publicId={uploadedImagePublicId} height='200' width="400" crop="scale" />
                         </CloudinaryContext>
                     ) : (
-                        <Image className='d-block my-3 posted-img' height='20%' width="100%" crop="scale" src={imageUrl} />
+                        <Image className='d-block my-3 posted-img' height='20%' width="100%" crop="scale" src='https://res.cloudinary.com/practicaldev/image/fetch/s--gIvrKWQi--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/5rbe6wwa5mcc2q4me43s.png' />
                     )}
                 </div>
                 <div className="text-editor container">
@@ -201,16 +209,17 @@ const TextEditor = () => {
                     />
                     <div className="publish-post mt-5">
                         <button type='submit' style={{ height: '3rem', marginTop: '4rem' }} className='btn btn-primary'>
-                            {articleStatePublished ? 'Published 🎉' : 'Publish Article'}
-                            {loaderState && (
+                            {articleStatePublished ? 'Published \uD83C\uDF89' : 'Publish Article'}
+                            {loaderState ? (
                                 <Spinner
                                     className='mx-2'
                                     as="span"
                                     animation="border"
                                     size="sm"
                                     role="status"
-                                    aria-hidden="true" />
-                            )}
+                                    aria-hidden="true"
+                                />
+                            ) : ""}
                         </button>
                     </div>
                     <div className="preview my-4">
