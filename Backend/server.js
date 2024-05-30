bcace88680698150c6d13e8225b9a5cdd0e174c7
Sahilll15 <sahilchalke1011@gmail.com>
2024-05-30 23:41:46 +0530

    port changed

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 const apiRoutes = require("./routes/ApiRoutes")
 require("dotenv").config()
 var helmet = require('helmet')
