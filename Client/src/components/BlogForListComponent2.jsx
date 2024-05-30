77713b198db0635f4a24c015e5ef37dfecb9a800
Varsani2520 <varsaniranjani2520@gmail.com>
2024-05-30 18:49:44 +0530

    added tags with style completed

--- Client/src/components/BlogForListComponent2.jsx
+++ Client/src/components/BlogForListComponent2.jsx
@@ -3,24 +3,16 @@ import { Link } from 'react-router-dom'
 
 const BlogForListComponent2 = ({ post }) => {
     // Define default values
-    const defaultImage = 'https://via.placeholder.com/150';  // Replace with your default image URL
     const defaultTitle = 'Blog website';
-    const defaultDescription = ' description for the blog post.';
+    const defaultDescription = 'Description for the blog post.';
 
     return (
-        <div className='blog-list-2' style={{ width: '50%' }}>
+        <div className='blog-list-2' style={{ width: '100%' }}>
             <div className="mx-3 mt-4 p-3">
                 <div style={{ color: '#0066FF' }} className='pb-2 card-subheading '>
                     <Link to="/">
                         Blog Article
                     </Link>
-                </div>
-                <div className="blog-image">
-                    <img 
-                        src={post.images ? post.images : defaultImage} 
-                        alt={post.title || defaultTitle} 
-                        style={{ width: '100%', height: 'auto' }} 
-                    />
                 </div>
                 <div className="card-title text-left">
                     <h5>
@@ -32,9 +24,33 @@ const BlogForListComponent2 = ({ post }) => {
                 <div className='card-description' style={{ fontSize: '0.95rem' }}>
                     {post?.description || defaultDescription}
                 </div>
+                <div className="card-tags mt-3">
+                    {post?.tags && post.tags.length > 0 ? (
+                        post.tags.map((tag, index) => (
+                            <span key={index} className='tag-badge' style={tagStyle}>
+                                {tag}
+                            </span>
+                        ))
+                    ) : (
+                        <span className='tag-badge' style={tagStyle}>
+                            Default Tag
+                        </span>
+                    )}
+                </div>
             </div>
         </div>
     )
+}
+
+// Inline styles for tags
+const tagStyle = {
+    display: 'inline-block',
+    backgroundColor: '#f1f1f1',
+    color: '#333',
+    borderRadius: '5px',
+    padding: '5px 10px',
+    margin: '5px',
+    fontSize: '0.85rem'
 }
 
 export default BlogForListComponent2
